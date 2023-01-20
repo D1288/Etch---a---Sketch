51417a9a8a7f6b01cfb89f9dc094dfc80707ce09
Daniel Metz <107957273+D1288@users.noreply.github.com>
2023-01-20 14:54:30 -0800

    Added full functionality to grid size, color picker, and reset button. Opacity slider not currently working

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,58 @@
 const container = document.querySelector(".gridcontainer")
 const squares = document.querySelectorAll(".squares")
+const userInput = document.querySelector('.userinput');
+const gridcontainer = document.querySelector('.gridcontainer');
+const reset = document.querySelector('.reset')
+let colorChange = document.querySelector(".colorpicker")
+let opacityController = document.querySelector(".opacity")
 
-for (let i = 0; i < 16; i++) {
-    const divs = document.createElement("div");
-    divs.classList.add("squares")
-    container.appendChild(divs);
-}
 
-squares.forEach((square) => {
-    square.addEventListener('onmouseover', () => {
-        squares.style.backgroundColor = "red";
-    });
-});
+
+userInput.addEventListener('click', () => {
+    gridcontainer.textContent = " "
+})
+
+userInput.addEventListener('click', () => {
+
+
+    let gridSize = prompt("Please enter the size of the grid. Max size is 100px")
+    while (gridSize > 100) {
+        gridSize = prompt("Please enter the size of the grid. Max size is 100px");
+    }
+    // return gridSize
+
+
+
+
+    let totalGrid = gridSize * gridSize
+    for (let i = 0; i < totalGrid; i++) {
+        const divs = document.createElement("div");
+        divs.classList.add("squares");
+        container.appendChild(divs);
+        
+        colorChange.addEventListener('input', () => {
+            divs.addEventListener("mouseover", () => {
+                divs.style.backgroundColor = colorChange.value
+            })
+        })
+        reset.addEventListener('click', () => {
+            divs.style.backgroundColor = "white";
+        })
+        
+        
+    }
+
+    gridcontainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridcontainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+
+})
+
+
+
+
+// opacityController.addEventListener('input', () =>{
+//     colorChange = colorChange.style.opacity
+// })
+
+
